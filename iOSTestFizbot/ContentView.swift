ca6509b666ae5b7eab3d538d6a4a60d374b30426
Alkın Çakıralar <a.cakiralar@gmail.com>
2023-07-11 10:07:20 +0300

    Update ContentView.swift

--- iOSTestFizbot/ContentView.swift
+++ iOSTestFizbot/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worlasdadsadasd!")
+            Text("Hello, worljkhjghfasdadsadasd!")
             Button("CLICK") {
             }
         }
